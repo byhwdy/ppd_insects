5e535de1c8b672499c0df156b286c37b32fe7617
zhengshaohua <zhengshaohua@58.com>
2020-03-27 23:15:03 +0800

    第一次提交：edit

--- tools/eval.py
+++ tools/eval.py
@@ -16,6 +16,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import sys
+sys.path.append('/paddle/ppd_insects')
+
 import os
 
 
